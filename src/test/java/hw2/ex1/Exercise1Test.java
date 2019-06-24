4af69c8bf5590b4103aa088bd850b384284a732a
Khodakovsky, Dmitry <Dmitrii.Khodakovsky@Clarivate.com>
2019-06-25 00:00:09 +0300

    Added comments for the HW2 #1

--- src/test/java/hw2/ex1/Exercise1Test.java
+++ src/test/java/hw2/ex1/Exercise1Test.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 
+// TODO What is the length line of code in Java code convention?
 public class Exercise1Test extends BasicMethods {
     @Test
     public void exercise1Test() {
@@ -50,6 +51,7 @@ public class Exercise1Test extends BasicMethods {
         //made in tearDown();
     }
 
+    // TODO What is the difference between current method and checkTextOnIndexPageUnderIcons?
     private void checkHeaderSectionElements() {
         List<String> headers = Arrays.asList("HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS");
         List<WebElement> headerElements = driver.findElements(By.cssSelector("[class='uui-navigation nav navbar-nav m-l8']>li"));
@@ -65,6 +67,7 @@ public class Exercise1Test extends BasicMethods {
         checkListWebElementsIsDisplayed(imageElements);
     }
 
+    // TODO What is the difference between current method and checkHeaderSectionElements?
     private void checkTextOnIndexPageUnderIcons() {
         List<String> benefitText = Arrays.asList("To include good practices\nand ideas from successful\nEPAM project",
                 "To be flexible and\ncustomizable", "To be multiplatform", "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…");
@@ -74,6 +77,7 @@ public class Exercise1Test extends BasicMethods {
         checkListWebElementsHaveProperText(textElements, benefitText);
     }
 
+    // TODO What is the difference between current method and checkHeaderSectionElements?
     private void checkMainHeaders() {
         List<String> textOnMainHeaders = Arrays.asList("EPAM FRAMEWORK WISHES…", "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO CONSEQUAT DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.");
         List<WebElement> headerElements = new ArrayList<>();
